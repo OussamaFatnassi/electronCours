f0c8df721f54d3dbaaec5b08ca86493a7b8e9e35
Oussama Fatnassi <oussamafatnassi@icloud.com>
2024-05-16 14:27:55 +0200

    chore: Update test results artifact name and path

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,4 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts"],
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "html"],
-  coveragePathIgnorePatterns: ["/node_modules/"],
-  moduleFileExtensions: ["ts", "js"],
-  transform: {
-    "^.+\\.ts$": "ts-jest",
-  },
 };
